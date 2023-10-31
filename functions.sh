ab537bf0775befabb98a96d78f93a6802e35b6b1
martimsantos <santos.martim@ua.pt>
2023-10-31 16:19:08 +0000

    dpawjda

--- functions.sh
+++ functions.sh
@@ -1,12 +1,16 @@
 function spacecheck() {
     padrao=""
+    sum="0"
     for ((i=1; i<=$#; i++)); do
         case "${!i}" in
             "-n")
                 # Acesse o próximo argumento usando $((i+1))
                 padrao="${!((i+1))}"
                 if [ -n "$padrao" ]; then
-                    
+                    for folder in $@; do 
+                        if [-d folder]; then 
+                        find $folder -name "$padrao" | du -b "$folder"
+                    done
                 fi
                 i=$((i+1)) # Avance para o próximo argumento
                 ;;
