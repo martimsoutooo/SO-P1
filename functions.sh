7c7f12ef11265f6ee72e59329b58c2983719abf3
Henriqueee <henriqueft04@ua.pt>
2023-11-01 13:44:34 +0000

    esta merda esta toda mal

--- functions.sh
+++ functions.sh
@@ -1,17 +1,11 @@
 
-function name_filter() {
-    repository="$1"
-    padrao="$2"
-
-
-    find "$repository" -type f -name "*$padrao*" | while read -r file; do
-        dir=$(dirname "$file") # todos
-        size=$(du -sh "$dir" | cut -f1)
-        echo "$size $dir"
-        
-    done
-
-}
+padrao=""
+data="2050-01-01"
+na=0
+da=0
+sa=0
+ra=0
+aa=0
 
 function spacecheck() {
     if [ $# -lt 1 ]; then
@@ -22,12 +16,6 @@ function spacecheck() {
         echo "O último argumento NÃO é um diretório: ${!#}"
     fi
         
-    padrao=""
-    na=1
-    da=0
-    sa=0
-    ra=0
-    aa=0
     
     for ((i=1; i<=$#; i++)); do
         case "${!i}" in
@@ -35,6 +23,7 @@ function spacecheck() {
                 if [ $((i + 1)) -le $# ]; then
                     temp=$((i+1))
                     padrao="${!temp}"
+                    echo "${padrao}"
 
                     na=1
                     #DAR SKIP AO PRÓXIMO ARGUMENTO PORQUE VAI SER O REGEX
@@ -47,6 +36,18 @@ function spacecheck() {
                 ;;
             "-d")
                 # data máxima de modificação
+                if [ $((i + 1)) -le $# ] && [[ "${!i}" == '"'* ]]; then
+                    data="${!i}"
+                    shift
+                    echo "${data}"
+                    data_check "$data"
+                    da=1
+                    # SKIP TO THE NEXT ARGUMENT BECAUSE IT WILL BE A DATE
+                    i=$((i + 1))
+                else
+                    echo "Missing date argument for -d option."
+                    exit 1
+                fi
                 ;;
             "-s")
                 # tamanho mínimo
@@ -70,4 +71,28 @@ function spacecheck() {
     repository="${!#}" 
 
     name_filter "$repository" "$padrao" 
+}
+
+function name_filter() {
+    repository="$1"
+    padrao="$2"
+
+
+    find "$repository" -type f -name "*$padrao*" | while read -r file; do
+        dir=$(dirname "$file") # todos
+        size=$(du -sh "$dir" | cut -f1)
+        echo "$size $dir"
+        
+    done
+
+}
+
+function data_check(){
+    local date_str="$1"
+    date -d "$date_str" >/dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        echo "Valid date"
+    else
+        echo "Invalid date"
+    fi
 }
