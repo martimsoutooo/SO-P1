b15dd3e24dac828bf069e0141f6524bcc5d6db65
martimsantos <santos.martim@ua.pt>
2023-11-02 16:57:29 +0000

    functions quase feito

--- functions.sh
+++ functions.sh
@@ -17,7 +17,7 @@ function spacecheck(){
         fi
     done
 
-    while getopts ":n:d:s:ral:" opt; do 
+    while getopts "n:d:s:l:ra" opt; do 
         case $opt in
             n)
                 regex=$OPTARG
@@ -46,30 +46,32 @@ function spacecheck(){
         esac
     done
 
+    table_header_print $@
     name_filter "$target_directory" "$regex"
     size_filter "$target_directory" "$minsize"
     date_filter "$target_directory" "$lastdate"
-    alphabetic_filter "$target_directory"                     
+    alphabetic_order "$target_directory"                     
 }
 
 function name_filter() {
     repository="$1"
     padrao="$2"
 
-    if [ $na -eq 1 ]; then
-        
+    if [ $na -eq 1 ]; then    
+
+        # Only search within the given directory, not subdirectories
         while IFS= read -r -d '' k; do
             size=0
-            folder=$(echo "$k" | grep -P -o '(?<=\.\.\/).*')
-            echo "Folder: $folder"
-
             while IFS= read -r -d '' i; do
                 size_i=$(du -b "$i" | cut -f1)
                 size=$(($size+$size_i))
             done < <(find "$k" -type f -regex ".*$padrao.*" -print0)
-            echo "Size: $size"
+            
+            table_line_print $size $k
         done < <(find "$repository" -type d -print0)
+        
     fi
+
 }
 
 function date_filter() {
@@ -77,34 +79,32 @@ function date_filter() {
     user_date="$2"
 
     if [ $da -eq 1 ]; then
-
+        
+        user_date_formatted=$(date -d "$user_date" "+%Y-%m-%d")
+        
         while IFS= read -r -d '' k; do
 
             size=0
             folder=$(echo "$k" | grep -P -o '(?<=\.\.\/).*')
-            echo "Folder: $folder"
-
+            
             while IFS= read -r -d '' i; do
                 
-                file_date_seconds=$(date -r "$i" "+%s")
-                user_date_seconds=$(date -d "$user_date" +%s)
+                file_date=$(date -r "$i" "+%Y-%m-%d")
 
+                file_date_seconds=$(date -r "$i" +%s)
+                user_date_seconds=$(date -d "$user_date_formatted" +%s)
+                
                 if [[ "$file_date_seconds" -le "$user_date_seconds" ]]; then
-                    
                     size_i=$(du -b "$i" | cut -f1)
                     size=$(($size+$size_i))
-
                 fi
-
             done < <(find "$k" -type f -print0)
 
-            echo "Size: $size"
-
+            table_line_print $size $k
         done < <(find "$repository" -type d -print0)
-
-    fi 
-
+    fi
 }
+
 
 
 function size_filter() {
@@ -113,25 +113,26 @@ function size_filter() {
 
     if [ $sa -eq 1 ]; then
 
-        while IFS= read -r -d '' k; do
-            
+        while IFS= read -r -d '' k; do 
+        # O -R CERTIFICA QUE A BACKSLASH É TRATADA COMO CHARACTER E NAO ESCAPE
+        # O -D DIZ QUE O READ É DELIMITADO POR UM NULL \0
+        # ISTO PERMITE TRATAR CORRETAMENTE DE DIRETÓRIOS COM ESCAÇOS NELES
             size=0
-            folder=$(echo "$k" | grep -P -o '(?<=\.\.\/).*')
-            echo "Folder: $folder"
-
+            
             while IFS= read -r -d '' i; do
                 size_i=$(du -b "$i" | cut -f1)
 
-                    if [ $size_i -ge $minsize ]; then
-                        size=$(($size+$size_i))
-                    fi
+                if [ $size_i -ge $minsize ]; then
+                    size=$(($size+$size_i))
+                fi
             done < <(find "$k" -type f -print0)
-            
-            echo "Size: $size"
 
+            table_line_print $size $k
         done < <(find "$repository" -type d -print0)
 
-    fi 
+        # EXECUTA O COMANDO E LE O OUTPUT COMO SE FOSSE UMA LINHA
+        # < QUER LER UM FICHEIRO, <() METE O CONTENT DOS ()A SER LIDOS COMO FILE
+    fi
 }
 
 function is_regex() {
@@ -144,11 +145,43 @@ function is_regex() {
     fi
 }
 
-function alphabetic_filter(){
+function alphabetic_order(){
 
     repository="$1"
     if [ $aa -eq 1 ]; then
-        echo "ALPHABETIC ORDER $repository"
-        find "$repository" -type d | sort
+        find "$repository" -type d | sort | xargs -I {} du -sb {} | while read -r line; do    
+            size=$(echo "$line" | awk '{print $1}')
+            folder=$(echo "$line" | awk '{print $2}')
+            table_line_print "$size" "$folder"
+        done
     fi
 }
+
+function table_header_print() {
+    
+    header="SIZE NAME $(date +'%Y%m%d') "
+    printf "%-10s %-5s %-10s" $header
+    
+    for ((i = 1; i <= $# - 1; i++)); do
+        if [[ "${!i}" =~ ^[0-9]+$ ]]; then
+            # Handle numeric arguments differently (without quotes)
+            printf " %s" "${!i}"
+        elif is_regex "${!i}"; then
+            printf " \"%-4s\"" "${!i}"
+        else
+            printf "%3s" "${!i}"
+        fi
+    done
+    
+    printf "%3s \n" $(basename "${!#}") 
+
+}
+
+function table_line_print(){
+    
+    size="$1"
+    folder=$(echo "$2" | grep -P -o '(?<=\.\.\/).*')
+
+    printf "%-10s %-5s \n" "$size" "$folder"
+}
+
