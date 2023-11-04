30d9081c4f83bb972e0ae47ae20d2c61b9e71329
Henriqueee <henriqueft04@ua.pt>
2023-11-04 14:49:22 +0000

    date tentativa implementacao

--- functions_tries.sh
+++ functions_tries.sh
@@ -159,6 +159,67 @@ function size_filter() {
     fi
 }
 
+function date_filter() {
+    repository="$1"
+    user_date_seconds="$2"
+    dc=1
+    declare -A passed_date
+
+    if [ $da -eq 1 ]; then
+
+        if [ ${#passed_filters[@]} -eq 0 ]; then
+        
+            while IFS= read -r -d '' k; do
+
+                size=0
+                folder_files=()
+                
+                while IFS= read -r -d '' i; do
+                    
+                    file_date=$(date -r "$i" "+%Y-%m-%d")
+                    file_date_seconds=$(date -r "$i" +%s)
+                    
+                    if [[ "$file_date_seconds" -le "$user_date_seconds" ]]; then
+                        size_i=$(du -b "$i" | cut -f1)
+                        size=$(($size+$size_i))
+                        folder_files+=("$i")
+                    fi
+                done < <(find "$k" -type f -print0)
+
+                passed_date["$k"]=$(IFS=,; echo "${folder_files[*]}")
+                associative["$k"]="$size"
+
+            done < <(find "$repository" -type d -print0)
+        else
+            for folder in "${!passed_filters[@]}"; do
+                size=0
+                array_string="${passed_filters[$folder]}"
+                IFS=, read -ra folder_files <<< "$array_string"
+                for j in "${folder_files[@]}"; do
+                    file_date=$(date -r "$j" "+%Y-%m-%d")
+                    file_date_seconds=$(date -r "$j" +%s)
+                    if [[ "$file_date_seconds" -le "$user_date_seconds" ]]; then
+                        size_i=$(du -b "$j" | cut -f1)
+                        size=$(($size+$size_i))
+                        folder_files+=("$j")
+                    fi
+                done
+
+                passed_date["$folder"]=$(IFS=,; echo "${folder_files[*]}")
+                associative["$folder"]="$size"
+            done
+            
+        fi
+        for i in "${!passed_date[@]}"; do
+            if [ -z "${passed_filters[$i]}" ]; then
+                passed_filters["$i"]=""
+            fi
+            passed_filters["$i"]+="${passed_date[$i]}"
+        done
+        table_line_print
+    fi
+}
+
 function table_header_print() {
     
     header="SIZE NAME $(date +'%Y%m%d') "
@@ -222,6 +283,7 @@ function is_date() {
     fi
 }
 
+# ESTA FUNCAO ESTÁ MAL!!!!!!!!!!!!!!!!!
 function is_regex() {
     local pattern="$1"
 
@@ -242,13 +304,13 @@ function is_number() {
     fi
 }
 
-
-
 for dir in "$@"; do
     if [ -d "$dir" ]; then
         target_directory="$dir"
     fi
 done
+
+extras_podres=()
 
 while getopts "n:d:s:l:ra" opt; do
     case $opt in
@@ -258,25 +320,34 @@ while getopts "n:d:s:l:ra" opt; do
                 na=1
                 nc=0
             else
-                echo "Missing or invalid regular expression argument for -n option."
+                echo -e "Missing or invalid regular expression argument for -n option\n Argument must be a valid regular expression"
                 exit 1
             fi
             ;;
         d)
-            date="$OPTARG"
-            if is_date "$date"; then
-                echo "$date"
-                da=1
+            lastDate=$OPTARG
+            echo $lastDate
                 
-            else
-                echo "Missing or invalid date argument for -d option."
+            # Verifica se a data fornecida é válida
+            if date -d "$lastDate" >/dev/null 2>&1; then                                       
+                lDate=$(date --date="$lastDate" +"%s")  # Converte a data para segundos desde a época
+                da=1     
+                dc=0                                
+            else 
+                echo "Erro: Data de início inválida"
                 exit 1
             fi
             ;;
         s)
             minsize="$OPTARG"
-            sa=1
-            sc=0
+            if is_number "$minsize"; then
+                sa=1
+                sc=0
+            else
+                echo -e "Missing or invalid size argument for -s option\n Argument must be a positive integer"
+                exit 1
+            fi
+            
             ;;
         r)
             # ordem inversa
@@ -291,15 +362,31 @@ while getopts "n:d:s:l:ra" opt; do
             n_lines="$OPTARG"
             if is_number "$n_lines"; then
                 max="$n_lines"
+            else
+                echo -e "Missing or invalid number of lines argument for -l option\n Argument must be a positive integer"
+                exit 1
             fi
             ;;
-        *)
+        \?)
             echo "Deu merda mano"
+            exit 1
             ;;
     esac
 done
 
+shift $((OPTIND-1))
+for arg in "$@"; do
+    extras_podres+=("$arg")
+done
+
+if [ ${#extras_podres[@]} -gt 1 ]; then
+    echo "Invalid argument(s): ${extras_podres[@]}"
+    exit 1
+fi
+
 table_header_print $@
-no_argument "$target_directory"
+
 name_filter "$target_directory" "$regex"
 size_filter "$target_directory" "$minsize"
+date_filter "$target_directory" "$lDate"
+no_argument "$target_directory"
